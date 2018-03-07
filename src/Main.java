7f1a4ab8efc510ffe9a2c48de3c12abd71b37ecc
Mats Bergstroem <hackingtrom@gmail.com>
2018-03-07 11:39:48 +0100

    Modify logging properties

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-	
+
 	static Logger logger = Logger.getGlobal();
 
 	public static void main(String[] args) {
-		
+
 		logger.log(Level.SEVERE, "Severe event");
+		logger.log(Level.INFO, "INFO event");
+		logger.log(Level.FINER, "FINER event.");  // not logged due to current logging.properties
 		AnotherClass ac = new AnotherClass();
 		ac.testMethod();
 
